bd967d280abf9ce380470ddda2f19b0d122c1969
Jacob Bashista <tgb.bashista@gmail.com>
2021-07-12 09:59:27 -0400

    File merging and S3 uploading

--- converter.py
+++ converter.py
@@ -1,6 +1,9 @@
+import botocore, boto3
 from pydub import AudioSegment
 import os, datetime
 
+
+SEP_INTERVAL = 1800
 
 recordings = {}
 
@@ -26,6 +29,11 @@ for name in recordings.keys():
     recordings[name] = files
 
 # Split list by times
+
+exports = []
+
+exportIndex = -1
+
 for name in recordings.keys():
     for i in range(len(recordings[name])):
         prev = None
@@ -35,27 +43,59 @@ for name in recordings.keys():
 
         # If there is nothing beforehand
         if prev == None:
-            # Start a new recording
-            pass
+            # Start a new file
+            exportIndex += 1
+            exports.append([recordings[name][i]])
         # If more than 30 minutes have passed
-        elif int(curr.split("_")[2].split(".")[0]) - int(prev.split("_")[2].split(".")[0]) > 1800:
-            # Start a new recording
-            pass
+        elif int(curr.split("_")[2].split(".")[0]) - int(prev.split("_")[2].split(".")[0]) > SEP_INTERVAL:
+            # Start a new file
+            exportIndex += 1
+            exports.append([recordings[name][i]])
         else:
-            # Keep recording
-            pass
-
-# epoch_time = int(file.split("_")[2].split('.')[0])
-# datetime_time = datetime.datetime.fromtimestamp(epoch_time)
-
-# Check if interval already exists on S3
-
-# If not upload to S3
+            # Keep same file
+            exports[exportIndex].append(recordings[name][i])
 
 
-# Audio Merge Example
-# sound1 = AudioSegment.from_wav("/path/to/file1.wav")
-# sound2 = AudioSegment.from_wav("/path/to/file2.wav")
+for export in exports:
+    firstFile = export[0]
+    lastFile = export[len(export) - 1]
+    name = firstFile.split("_")[0]
 
-# combined_sounds = sound1 + sound2
-# combined_sounds.export("/output/path.wav", format="wav")
+    firstTime = int(firstFile.split("_")[2].split(".")[0])
+    lastTime = int(lastFile.split("_")[2].split(".")[0])
+
+    firstDate = str(datetime.datetime.fromtimestamp(firstTime))
+    lastDate = str(datetime.datetime.fromtimestamp(lastTime))
+
+    recordingName = name + "_" + firstDate + "-" + lastDate + ".wav"
+    
+    audioTrack = None
+    for track in export:
+        sound = AudioSegment.from_wav(track)
+        if audioTrack == None:
+            audioTrack = sound
+        else:
+            audioTrack += sound
+    audioTrack.export("./exports/" + recordingName, format="wav")
+    print("Exported audio for " + name + " that was " + str(len(audioTrack)/1000/60) + " minutes long")
+
+# Connect to S3
+s3 = boto3.resource('s3')
+
+# Upload to S3
+for file in os.listdir('./exports'):
+    try:
+        s3.Object('galeforce-audio', file).load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            # File does not exist on S3 yet
+            print('Uploading audio ' + file)
+            data = open('./exports/' + file, 'rb')
+            s3.Bucket('galeforce-audio').put_object(Key=file, Body=data)
+            print('Uploaded audio ' + file)
+        else:
+            print('Error when connecting to S3')
+            raise
+    else:
+        # File is already on S3
+        print(file + ' already uploaded, skipping')
